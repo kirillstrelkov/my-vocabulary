af28e8bf396378062a82543c2788a5b5e3950e37
Kirill Strelkov <kirillstrelkov@users.noreply.github.com>
2016-10-14 22:55:13 +0200

    added @selenium tag for Facebook test

--- features/login.feature
+++ features/login.feature
@@ -1,5 +1,6 @@
 Feature: User should be able to login
 
+  @selenium
   Scenario: User logins in with Facebook account
     Given I am on main page
     When I login with "Facebook" account
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -11,9 +11,14 @@ After do |scenario|
 end
 
 Before do |scenario|
+  if scenario.source_tag_names.include?('@selenium') && $driver != :selenium
+    skip_this_scenario("Scenario should be used with #{$driver} driver")
+  end
+
   if scenario.source_tag_names.include?('@mobile') && !$mobile_drivers.include?($driver)
     skip_this_scenario("Mobile scenario cannot be used with #{$driver} driver")
   end
+
   $exec_once = $exec_once || false
   current_window.maximize if !$exec_once && Capybara.default_driver == :selenium
 end
