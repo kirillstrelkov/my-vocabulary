28d7dc4fcd48f64aeb1bc72214e9a8fcd971a978
Kirill Strelkov <kirillstrelkov@users.noreply.github.com>
2016-11-08 23:47:36 +0100

    small fix for words_helper

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,9 +1,9 @@
 module WordsHelper
   def format_text(word, text)
-    lang_code = word.send("lang_code#{text.to_s[/\d+/]}").to_sym
-    gender = word.send("#{text}_gender")
-    text = word.send(text.to_s)
-    if gender
+    gender = word["#{text}_gender"]
+    lang_code = word["lang_code#{text.to_s[/\d+/]}"].to_sym
+    text = word[text.to_s]
+    if gender && !gender.empty?
       if lang_code == :de
         subs = { n: 'das', f: 'die', m: 'der' }
         gender = subs[gender.to_sym]
